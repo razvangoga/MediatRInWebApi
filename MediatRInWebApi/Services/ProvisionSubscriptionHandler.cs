d60469a2041a6c9c6b3de12829ab1170b476fb7f
Razvan Goga <razvan@avalnet.ro>
2021-02-26 19:40:47 +0100

    implemented around the "Cannot access a disposed object.Object name: 'IServiceProvider'." exception

--- MediatRInWebApi/Services/ProvisionSubscriptionHandler.cs
+++ MediatRInWebApi/Services/ProvisionSubscriptionHandler.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace MediatRInWebApi.Services
@@ -14,15 +16,15 @@ namespace MediatRInWebApi.Services
     
     public class ProvisionSubscriptionHandler : INotificationHandler<ProvisionSubscriptionRequest>
     {
-        private readonly IMediator _mediator;
         private readonly ILogger<ProvisionSubscriptionHandler> _logger;
         private readonly ISignalRService _signalRService;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
 
-        public ProvisionSubscriptionHandler(IMediator mediator, ILogger<ProvisionSubscriptionHandler> logger, ISignalRService signalRService)
+        public ProvisionSubscriptionHandler(ILogger<ProvisionSubscriptionHandler> logger, ISignalRService signalRService, IServiceScopeFactory serviceScopeFactory)
         {
-            this._mediator = mediator;
             this._logger = logger;
             this._signalRService = signalRService;
+            this._serviceScopeFactory = serviceScopeFactory;
         }
         
         public async Task Handle(ProvisionSubscriptionRequest request, CancellationToken cancellationToken)
@@ -38,7 +40,13 @@ namespace MediatRInWebApi.Services
 
                 if (request.Crash)
                     throw new Exception("ka-boom");
-            
+
+                using (IServiceScope scope = this._serviceScopeFactory.CreateScope())
+                {
+                    IMediator mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+                    await mediator.Publish(new Ok(), cancellationToken);
+                }
+                
                 this._signalRService.Ok(request.SubscriptionId);
             
                 this._logger.LogDebug("ProvisionSubscriptionHandler end ok");
---
+++ MediatRInWebApi/Services/SignalRNotificationService.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace MediatRInWebApi.Services
+{
+    public class Ok : INotification
+    {
+        
+    }
+    
+    public class SignalRNotificationService : INotificationHandler<Ok>
+    {
+        private readonly ILogger<SignalRNotificationService> _logger;
+
+        public SignalRNotificationService(ILogger<SignalRNotificationService> logger)
+        {
+            this._logger = logger;
+        }
+        
+        public Task Handle(Ok notification, CancellationToken cancellationToken)
+        {
+            this._logger.LogDebug($"SignalRNotificationService {nameof(Ok)}");
+            return Task.CompletedTask;
+        }
+    }
+}
